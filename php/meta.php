559fdbc0ad71789e11d66f534dbae0036691f46c
Igor Padoim <igorpadoim@gmail.com>
2023-05-11 18:49:36 -0300

    Update app metadata

--- php/meta.php
+++ php/meta.php
@@ -1,9 +1,9 @@
 <?php
-define("APP_NAME",          "Poo");
-define("APP_REPO",          "http://pudim.com.br/");
+define("APP_NAME",          "Adventure");
+define("APP_REPO",          "https://github.com/Nereare/adventure");
 define("APP_AUTHOR",        "Igor Padoim");
 define("APP_YEAR",          "2023");
-define("APP_BIRTHDAY",      "2023-03-29");
-define("APP_LICENSE_NAME",  "Licença Hipocrática HL3-FULL");
+define("APP_BIRTHDAY",      "2023-05-11");
+define("APP_LICENSE_NAME",  "Hippocratic License 3 - Full");
 define("APP_LICENSE_URI",   "https://firstdonoharm.dev/version/3/0/full.html");
 define("APP_VERSION",       "0.1.0");
