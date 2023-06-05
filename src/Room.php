3afd8ca50f47e40a366a9f979237fedf86e76b75
Igor Padoim <igorpadoim@gmail.com>
2023-06-05 19:13:34 -0300

    Add exits' methods

--- src/Room.php
+++ src/Room.php
@@ -33,6 +33,8 @@ class Room {
     $this->perception     = $perception;
     $this->investigation  = $investigation;
     $this->contents       = $contents;
+
+    $this->exits          = [];
   }
 
   /**
@@ -105,5 +107,57 @@ class Room {
    */
   public function get_contents(): \Nereare\Adventure\Loot|null {
     return $this->contents;
+  }
+
+  /**
+   * Retrieve the room's exits.
+   *
+   * @return array   The room's exits, if any, as an array of arrays, each composed of the room's IDs (item 0) and the exit's text for this room (item 1).
+   */
+  public function get_exits(): array {
+    return $this->exits;
+  }
+
+  /**
+   * Add a new room as an exit of the current one.
+   *
+   * @param string $room_id    The UUID for the exit room.
+   * @param string $room_txt   The text to be displayed.
+   * @return void
+   */
+  public function add_exit(string $room_id, string $room_txt) {
+    $this->exits[] = [$room_id, $room_txt];
+  }
+
+  /**
+   * Remove an exit from the room's exits by its internal index.
+   *
+   * @param integer $index   The internal integer index of this exit.
+   * @return void
+   */
+  public function remove_exit_by_index(int $index) {
+    unset($this->exits[$index]);
+  }
+
+  /**
+   * Remove an exit from the room's exits by its UUID.
+   *
+   * @param string $room_id   The UUID of this exit.
+   * @return boolean          `true` if the room is an exit and has been removed, `false` otherwise.
+   */
+  public function remove_exit_by_id(string $room_id): bool {
+    $return_index = null;
+    foreach ($this->exits as $index => $room) {
+      if ($room[0] == $room_id) {
+        $return_index = $index;
+      }
+    }
+
+    if ($return_index === null) {
+      return false;
+    } else {
+      $this->remove_exit_by_index($return_index);
+      return true;
+    }
   }
 }
