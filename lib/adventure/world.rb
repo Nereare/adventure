efc7c9049b6e488c450f54301660fe9dc49bb0fb
Igor Padoim <igorpadoim@gmail.com>
2026-04-30 21:04:48 -0300

    Begin loading and saving methods

--- lib/adventure/world.rb
+++ lib/adventure/world.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'psych'
+
+require_relative 'being'
+require_relative 'inventory'
+require_relative 'item'
+# require_relative 'player'
+require_relative 'purse'
 require_relative 'room'
 
 module Adventure
@@ -11,34 +18,81 @@ module Adventure
     attr_reader :subtitle
     # An Array of authors, in the format `Name <email> (url)`.
     attr_reader :authors
-    # The full name of the license this adventure is released under.
+    # The full name of the license the adventure is released under.
     attr_reader :license
-    # The year (or years) of development of this adventure.
+    # The year (or years) of development of the adventure.
     attr_reader :year
-    # A longer description of this adventure.
+    # A description of the adventure.
     attr_reader :description
-    # Number of players for which this adventure is developed.
-    attr_reader :players
-    # The initial level for the player(s).
-    attr_reader :starting_level
-    # Initially the **starting** room for this adventure, then, during the adventure execution, the algorithm will use as the actual current room for the party.
+    # The adventure's player object.
+    attr_reader :player
+    # The index of the game's current Room, initially the starting Room.
     attr_reader :current_room
     # A Hash of variables to be used by the adventure --- currently unused, kept as placeholder.
     attr_reader :global_vars
 
-    # TODO: Write documentation.
+    # Create a new game instance - a new World.
+    #
+    # @param  opts    [Hash]            A list of named parameters.
+    # @option opts    [String]          title           The adventure's title / name.
+    # @option opts    [String,nil]      subtitle        The adventure's subtitle / flavor text.
+    # @option opts    [Array<String>]   authors         An Array of authors, in the format `Name <email> (url)`.
+    # @option opts    [String]          license         The full name of the license the adventure is released under.
+    # @option opts    [String]          year            The year (or years) of development of the adventure.
+    # @option opts    [String]          description     A description of the adventure.
+    # @option opts    [Player]          player          The adventure's player object.
+    # @option opts    [Hash]            atlas           An Array of all the rooms for the adventure.
+    # @option opts    [Integer,Symbol]  current_room    The index of the game's current Room, initially the starting Room.
+    # @option opts    [Hash]            global_vars     A Hash of variables to be used by the adventure --- currently unused, kept as placeholder.
     def initialize(**opts)
-      @title          = opts.key?(:title) ? opts[:title].trim : ''
-      @subtitle       = opts.key?(:subtitle) ? opts[:subtitle].trim : ''
-      @authors        = opts.key?(:authors) ? opts[:authors].to_a : []
-      @license        = opts.key?(:license) ? opts[:license].trim : ''
-      @year           = opts.key?(:year) ? opts[:year].trim : ''
-      @description    = opts.key?(:description) ? opts[:description].trim : ''
-      @players        = opts.key?(:players) ? opts[:players].to_i : 1
-      @starting_level = opts.key?(:starting_level) ? opts[:starting_level].to_i : 1
-      @current_room   = opts.key?(:current_room) ? opts[:current_room] : nil
-      @current_room   = Room.new('Void', 'A large nothingness.') unless @current_room.is_a? Room
-      @global_vars    = opts.key?(:global_vars) ? opts[:global_vars].to_h : {}
+      @title        = opts.key?(:title) ? opts[:title].trim : ''
+      @subtitle     = opts.key?(:subtitle) ? opts[:subtitle].trim : nil
+      @authors      = opts.key?(:authors) ? opts[:authors].to_a : []
+      @license      = opts.key?(:license) ? opts[:license].trim : 'Creative Commons 0 1.0 Universal'
+      @year         = opts.key?(:year) ? opts[:year].trim : Date.today.year.to_s
+      @description  = opts.key?(:description) ? opts[:description].trim : ''
+      @player       = opts.key?(:player) ? opts[:player].to_i : nil
+      @atlas        = opts.key?(:atlas) ? opts[:atlas].to_a : []
+      @current_room = opts.key?(:current_room) ? opts[:current_room].to_i : 0
+      @global_vars  = opts.key?(:global_vars) ? opts[:global_vars].to_h : {}
+    end
+
+    # Class method
+
+    def self.load(file)
+      yaml = Psych.safe_load_file(
+        file,
+        permitted_classes: [
+          Adventure::Being,
+          Adventure::Inventory,
+          Adventure::Item,
+          # Adventure::Player,
+          Adventure::Purse,
+          Adventure::Room,
+          Adventure::World
+        ],
+        aliases: true
+      )
+      raise StandardError, 'Invalid input.' if yaml.nil?
+      yaml
+    end
+
+    # Instance methods
+
+    def save(file)
+      yaml = Psych.safe_dump(
+        self,
+        permitted_classes: [
+          Adventure::Being,
+          Adventure::Inventory,
+          Adventure::Item,
+          # Adventure::Player,
+          Adventure::Purse,
+          Adventure::Room,
+          Adventure::World
+        ]
+      )
+      File.write(file, yaml)
     end
 
     # TODO: continue World entity definitions.
